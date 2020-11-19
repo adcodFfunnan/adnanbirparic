286d7211cf47af9ec28e8fc60b9d15fd276a96b8
Adnan Birparic <adnan.birparic@gmail.com>
2020-11-19 14:22:37 +0100

    workingOn

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
-import react_puzzle from '../Files/Projects/react_puzzle.gif';
-
+import photo_1 from '../Files/Projects/photo_1.png';
+import photo_2 from '../Files/Projects/photo_2.png';
+import photo_3 from '../Files/Projects/photo_3.png';
+import photo_4 from '../Files/Projects/photo_4.png';
+import photo_5 from '../Files/Projects/photo_5.png';
+import photo_6 from '../Files/Projects/photo_6.png';
 
 
 
 function JustProjects(props) {
-
     let myProjects = [{
-        ProjectImg: "https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
-        ProjectGif: { react_puzzle },
-        description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
+        ProjectImg: photo_1,
+        description: "This is my portfolio website built with React JS.",
         group: "new",
         key: 1,
         skills: [" React JS", "CSS", "HTML"],
         title: "MyPortfolio"
     },
     {
-        ProjectImg: "https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
-        ProjectGif: { react_puzzle },
-        description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
+        ProjectImg: photo_2,
+        description: "It is a movie search application created using the React JS and The Movie Database API. The app allows users to search for movies or TV shows and provide detailed descriptions with trailers.",
         group: "new",
         key: 2,
         skills: [" React TS", "CSS", "HTML"],
         title: "FindMovie"
     },
     {
-        ProjectImg: "https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
-        ProjectGif: { react_puzzle },
+        ProjectImg: photo_3,
         description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
         group: "new",
         key: 3,
@@ -35,9 +35,8 @@ function JustProjects(props) {
         title: "PuzzleGame"
     },
     {
-        ProjectImg: "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-        ProjectGif: { react_puzzle },
-        description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
+        ProjectImg: photo_4,
+        description: "It is a multiple user application that allows users to transfer image files over the Internet. After loading the images, the application provides access key which is used to access to uploaded images. The list of uploaded images can be modified in terms of deleting or adding some images", 
         group: "old",
         key: 4,
         skills: [" React JS", "CSS", "HTML"],
@@ -45,9 +44,8 @@ function JustProjects(props) {
 
     },
     {
-        ProjectImg: "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-        ProjectGif: { react_puzzle },
-        description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
+        ProjectImg: photo_5,
+        description: "This app solves the problem with printing multiple drawings from AutoCad. If we have several AutoCad drawings ordered side-by-side, it takes time to print these drawings because each drawings has to be individually marked and printed. With the AutoCadPlot app it can be done in seconds.",
         group: "old",
         key: 5,
         skills: [" React JS", "CSS", "HTML"],
@@ -55,8 +53,7 @@ function JustProjects(props) {
 
     },
     {
-        ProjectImg: "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-        ProjectGif: { react_puzzle },
+        ProjectImg: photo_6,
         description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
         group: "old",
         key: 6,
@@ -66,7 +63,6 @@ function JustProjects(props) {
     },
     {
         ProjectImg: "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-        ProjectGif: { react_puzzle },
         description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
         group: "old",
         key: 7,
@@ -76,7 +72,6 @@ function JustProjects(props) {
     },
     {
         ProjectImg: "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-        ProjectGif: { react_puzzle },
         description: "This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices.",
         group: "old",
         key: 8,
@@ -93,11 +88,11 @@ function JustProjects(props) {
         <div key={project.key} className={props.classForProjects}>
             <div className="ProjectIMG">
                 <img className="ProjectImg" src={project.ProjectImg} />
-                <img className="ProjectGif" src={props.gif} />
-                <p className="ProjectTitle">{project.title}</p>
             </div>
             <div className="ProjDescr">
-                <p>{props.description}</p>
+                <p>{project.description}</p>
+                <p className="ProjectTitle">{project.title}</p>
+
             </div>
             <div className="ProjectButtons">
                 <a>GITHUB</a>
@@ -171,7 +166,7 @@ function Projects(props) {
             </div>
 
 
-            <JustProjects gif={react_puzzle}
+            <JustProjects 
                 image="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
                 description="This is a puzzle game built using React JS. The game provides the ability to insert the image you want to play with, and it is possible to determine the level of difficulty. It can be played on desktops and mobile devices."
                 showProjects={props.StyleProjectsOnScroll["projects"]}
